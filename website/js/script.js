bdea1eab5e715d378ba4252e722de4641a43bb8e
svasandani <vasandanishailesh@gmail.com>
2020-07-21 14:53:34 -0400

    Change static website to be separate from Golang templating

---
+++ website/js/script.js
@@ -0,0 +1,175 @@
+let form = document.querySelector("form");
+let card = document.querySelector(".sign-in-card")
+let urlParams = new URLSearchParams(window.location.search);
+let responseOK = false;
+
+const API = "http://localhost:3000/api/";
+
+const errorDict = {
+  "password too short; minimum 8 alphanumeric characters": "Your password is too insecure. Please use a password longer than 8 characters.",
+  "client could not be found": "Something went wrong. Try going back to the previous page and signing in again.",
+  "user could not be found": "Email or password incorrect. Please try again.",
+  "invalid field: email": "Your email appears to be invalid. Please try again.",
+  "required field missing: response_type": "Something went wrong. Try going back to the previous page and signing in again.",
+  "required field missing: client_id": "Something went wrong. Try going back to the previous page and signing in again.",
+  "required field missing: redirect_uri": "Something went wrong. Try going back to the previous page and signing in again.",
+  "required field missing: email": "You must enter an email. Please try again.",
+  "required field missing: password": "You must enter a password. Please try again."
+}
+
+function doReady() {
+  form.addEventListener("submit", (e) => {
+    handleSubmission(e)
+  }, false);
+}
+
+
+function handleSubmission(e) {
+  e.preventDefault();
+
+  let email = form.querySelector("#email").value;
+  let password = form.querySelector("#password").value;
+  // let confirmPassword = form.querySelector("#confirm-password").value;
+  //
+  // if (confirmPassword !== password) {
+  //   return;
+  // }
+
+  fetch(API + "auth", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "response_type": "code",
+        "redirect_uri": urlParams.get("redirect_uri"),
+        "client_id": urlParams.get("client_id"),
+        "state": urlParams.get("state"),
+        "user": {
+          "email": email,
+          "password": password
+        }
+      })
+    })
+    .then(response => {
+      if (response.status === 200) {
+        response = response.json();
+        responseOK = true;
+        return response;
+      } else {
+        response = response.json();
+        return response;
+      }
+    })
+    .then(data => authorizeWith(data));
+}
+
+function authorizeWith(data) {
+  if (responseOK) {
+    let redirectParams = new URLSearchParams("");
+    redirectParams.set("auth_code", data.auth_code);
+    redirectParams.set("state", data.state);
+
+    window.location = data.redirect_uri + "?" + redirectParams.toString();
+  } else {
+    if (errorDict[data.message] == undefined) {
+      let el = appendError(createError("An error occurred. Please try signing in again."));
+      setTimeout(() => {
+        removeError(el);
+      }, 4000);
+    } else {
+      let el = appendError(createError(errorDict[data.message]));
+      setTimeout(() => {
+        removeError(el);
+      }, 4000);
+    }
+  }
+}
+
+function removeErrors() {
+  let errors = document.querySelectorAll(".errors");
+  errors.forEach(error => {
+    card.removeChild(error);
+  });
+}
+
+function removeError(el) {
+  el.style.opacity = "50%";
+  setTimeout(() => {
+    el.style.opacity = "0%";
+    setTimeout(() => {
+      removeElement(el);
+    }, 300);
+  }, 3500);
+}
+
+function removeElement(el) {
+  el.parentNode.removeChild(el);
+}
+
+function createError(text) {
+  let el = document.createElement("div");
+  el.classList.add("errors", "card");
+  el.innerHTML = text;
+  return el;
+}
+
+function appendError(el) {
+  let errors = document.querySelectorAll(".errors");
+  if (errors.length > 2) {
+    removeElement(errors[errors.length - 1]);
+  }
+  let head = card.querySelector("h1");
+  head.after(el);
+  return el;
+}
+
+function loadMainElement(mode, service) {
+  let url = "";
+
+  if (mode == "register") {
+    url = "register.html";
+  } else {
+    url = "sign_in.html";
+  }
+
+  fetch(url).then(response => response.text()).then(data => {
+    let header = document.querySelector("header");
+    let main = document.querySelector("main");
+
+    data = data.replace("{{service}}", service);
+
+    if (main == null) {
+      header.insertAdjacentHTML("afterend", data);
+    } else {
+      document.body.removeChild(main);
+      header.insertAdjacentHTML("afterend", data);
+    }
+  })
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  fetch(API + "client/identify", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "client": {
+
+          "redirect_uri": urlParams.get("redirect_uri"),
+          "id": urlParams.get("client_id")
+        }
+      })
+    })
+    .then(response => {
+      if (response.status === 200) {
+        response = response.json();
+        return response;
+      } else {
+        document.querySelector("header").after(createError("Something went wrong. Please try signing in again."))
+      }
+    }).then(data => {
+      if (data) loadMainElement(urlParams.get("method"), data.client.name);
+    })
+});
