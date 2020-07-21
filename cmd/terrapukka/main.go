b8da01ceeef94d1e2c3c732d271a477a239d6c5d
svasandani <vasandanishailesh@gmail.com>
2020-07-21 14:07:03 -0400

    Remove website templating system

--- cmd/terrapukka/main.go
+++ cmd/terrapukka/main.go
@@ -7,7 +7,6 @@ import (
 
   "github.com/svasandani/terrapukka/internal/api"
   "github.com/svasandani/terrapukka/internal/db"
-  "github.com/svasandani/terrapukka/internal/web"
 )
 
 func main() {
@@ -22,17 +21,11 @@ func main() {
   database := db.ConnectDB(db.Connection { User: *dbuser, Password: *dbpass, Database: *dbname })
   defer database.Close()
 
-  web.Init()
-
   http.HandleFunc("/api/register", api.Middleware(api.CreateUserHandler))
   http.HandleFunc("/api/auth", api.Middleware(api.AuthorizeUserHandler))
 
   http.HandleFunc("/api/client/register", api.Middleware(api.CreateClientHandler))
-  // expect AUTH_CODE, CLIENT_ID, CLIENT_SECRET, and return name and email corresponding to AUTH_CODE
   http.HandleFunc("/api/client/auth", api.Middleware(api.AuthorizeClientHandler))
-
-  http.HandleFunc("/", web.Handler)
-  http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./web/static"))))
 
   log.Fatal(http.ListenAndServe(":" + *port, nil))
 }
